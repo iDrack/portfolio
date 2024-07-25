1961fe1bb71ed250ce1b8085d4b97886a390bba2
Thomas Dignoire <tdignoire@protonmail.com>
2024-07-26 01:02:55 +0200

    Ajout du bandeau de compétence

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ import withMT from "@material-tailwind/html/utils/withMT";
 
 export default {
   content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: true,
   theme: {
     fontFamily: {
-      sans: ['"inter"', 'sans-serif']
+      sans: ['"inter"', "sans-serif"],
     },
     extend: {},
     container: {
@@ -18,10 +18,20 @@ export default {
         from: { backgroundPosition: "0 0" },
         to: { backgroundPosition: "-200% 0" },
       },
+      shimmer: {
+        from: { backgroundPosition: "200% 0" },
+        to: { backgroundPosition: "-200% 0" },
+      },
+      "infinite-scroll": {
+        from: { transform: "translateX(0)" },
+        to: { transform: "translateX(-100%)" },
+      },
     },
     animation: {
       "text-shimmer": "text-shimmer 2.5s ease-out infinite alternate",
+      shimmer: "shimmer 32s ease-in-out infinite",
+      "infinite-scroll": "infinite-scroll 35s linear infinite",
     },
   },
   plugins: [],
-}
+};
